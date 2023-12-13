59e37be5f0b782ef45875378f3a5027b86d03d01
Abdul Azeez Kemp <aakemp786@gmail.com>
2023-12-13 11:10:43 +0200

    all

---
+++ src/components/Navbar.vue
@@ -0,0 +1,102 @@
+<template>
+    <div>
+        <nav>
+            <div class="navbar">
+                <div class="left">
+                    <div class="logo">
+                        <a href="">
+                            <img src="https://i.postimg.cc/mgn37vDw/aak-high-resolution-logo-black-on-transparent-background1.png"
+                                alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="right">
+                    <div class="menu">
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">About</a></li>
+                            <li><a href="">Education & Skills</a></li>
+                            <li><a href="">Projects</a></li>
+                            <li><a href="">Testimonials</a></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.navbar {
+    width: 100%;
+    height: 3rem;
+    display: flex;
+    position: fixed;
+    /* border-bottom: 1px solid red; */
+}
+
+.left {
+    width: 30%;
+    height: 100%;
+    /* border: 1px solid black; */
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    padding-left: 5rem;
+}
+
+.right {
+    width: 70%;
+    height: 100%;
+    /* border: 1px solid black; */
+}
+
+.left .logo {
+    height: 1.2rem;
+    width: 30%;
+    /* border: 1px solid green; */
+}
+
+.logo a img {
+    width: 100%;
+    height: 100%;
+}
+
+/* .logo a img:hover {
+    cursor: pointer;
+} */
+
+.right .menu {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.menu ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    display: flex;
+    gap: 2rem;
+}
+
+.menu ul li {
+    list-style: none;
+}
+
+.menu ul li a {
+    text-decoration: none;
+    color: black;
+    font-size: 1.1rem;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <div class="name">
+      <h1>ABDULAZEEZKEMP</h1>
+      <h1>ABDULAZEEZKEMP</h1>
+    </div>
+    <div class="img">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni recusandae assumenda pariatur? Qui molestiae
+      blanditiis cum, similique cumque ab animi, dolore inventore iusto, amet illo debitis ipsum quod delectus. Earum.
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+
+<style scoped>
+#home {
+  width: 100%;
+  height: 100%;
+  background-color: rgb(248, 248, 170);
+  display: flex;
+  justify-content: center;
+  align-items: end;
+  flex-direction: column;
+}
+
+.name {
+  font-size: 10rem;
+  width: 180vw;
+  height: 100vh;
+  overflow-y: scroll;
+  scroll-behavior: smooth;
+  text-wrap: none;
+  text-align: center;
+  padding-top: 10%;
+  display: flex;
+}
+
+.name::-webkit-scrollbar{
+  display: none;
+}
+
+.name h1{
+  animation: slider 10s infinite linear;
+  font-family: 'Righteous', sans-serif;
+}
+
+@keyframes slider{
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-100%);
+  }
+}
+</style>
